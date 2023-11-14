363251236d988f51ad8d3b55e2e96144c0e0851a
PERCABANGAN_CARLOS <bataraputra567@gmail.com>
2023-11-14 11:29:51 +0700

    first commit

---
+++ percabangan.php
@@ -0,0 +1,10 @@
+<?php
+    $usia = 16;
+    if($usia>=13 && $usia <=17){
+        echo "usia remaja";
+    }else if ($usia <13){
+        echo "usia anak-anak";
+    }else {
+        echo "usia dewasa";
+    }
+    ?>
